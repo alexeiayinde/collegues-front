402381c846c1b8871beb2e68f90f89704b2f4933
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-07-15 15:27:06 +0200

    création structure 'Collegue'

---
+++ src/app/models/Collegue.ts
@@ -0,0 +1,54 @@
+export class Collegue {
+    private _matricule?:string;
+    private _nom:string;
+    private _prenoms:string;
+    private _email:string;
+    private _dateDeNaissance:Date;
+    private _photoUrl:string;
+
+    constructor(matricule:string, nom:string, prenoms:string, email:string, dateDeNaissance:Date, photoUrl:string) {
+        this._matricule=matricule;
+        this._nom=nom;
+        this._prenoms=prenoms;
+        this._email=email;
+        this._dateDeNaissance=dateDeNaissance;
+        this._photoUrl=photoUrl;
+    }
+
+    get matricule() {
+        return this._matricule;
+    }
+    get nom() {
+        return this._nom;
+    }
+    get prenoms() {
+        return this.prenoms;
+    }
+    get email() {
+        return this.email;
+    }
+    get dateDeNaissance() {
+        return this.dateDeNaissance;
+    }
+    get photoUrl() {
+        return this.photoUrl;
+    }
+    set matricule(matricule:string) {
+        this._matricule = matricule;
+    }
+    set nom(nom:string) {
+        this._nom = nom;
+    }
+    set prenoms(prenoms:string) {
+        this._prenoms = prenoms;
+    }
+    set email(email:string) {
+        this._email = email;
+    }
+    set dateDeNaissance(dateDeNaissance:Date) {
+        this._dateDeNaissance = dateDeNaissance;
+    }
+    set photoUrl(photoUrl:string) {
+        this._photoUrl = photoUrl;
+    }
+}
