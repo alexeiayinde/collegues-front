9a8006b829fe4f02691fdebf665a71e9148ef4ca
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-07-19 11:07:13 +0200

    Angular TP 06 - Composant Collegue - Créer - optimisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CollegueComponent } from './collegue/collegue.component';
 import { RechercheCollegueParNomComponent } from './recherche-collegue-par-nom/recherche-collegue-par-nom.component';
-import { FormsModule } from '@angular/forms';
 import { CreerCollegueComponent } from './creer-collegue/creer-collegue.component';
 
 @NgModule({
@@ -14,7 +14,7 @@ import { CreerCollegueComponent } from './creer-collegue/creer-collegue.componen
     AppComponent,
     CollegueComponent,
     RechercheCollegueParNomComponent,
-    CreerCollegueComponent
+    CreerCollegueComponent,
   ],
   imports: [
     BrowserModule,
